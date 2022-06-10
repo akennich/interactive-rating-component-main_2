004463eb0352cd71c4a340deb4965c5e55a48e83
akennich <104636329+akennich@users.noreply.github.com>
2022-06-10 01:04:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const submit = document.querySelector('#submit');
+const labs = document.querySelectorAll('.lab');
 const selection = document.querySelector('#selection');
 const section1 = document.querySelector('#section1')
 const section2 = document.querySelector('#section2')
@@ -7,3 +8,15 @@ submit.addEventListener('click', function() {
     section1.classList.add('show');
     section2.classList.remove('show');
 })
+
+for (let i=0; i < labs.length; i++) {
+    labs[i].addEventListener('click', function(){
+        labs[i].classList.add('bcolor');
+        for(let j = 0; j < labs.length; j++) {
+            if(j !== i){
+                labs[j].classList.remove('bcolor');
+            }
+        }
+    })
+    
+}
